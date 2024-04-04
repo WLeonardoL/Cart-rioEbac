95e3b72601cdddd0bde8288bda5cd50ce896671a
WLeonardoL <166047813+WLeonardoL@users.noreply.github.com>
2024-04-04 14:10:06 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- Teste 2.cpp
+++ Teste 2.cpp
@@ -128,15 +128,15 @@ int main()
 		printf("Escolha a opção desejada do menu\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n");
+		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: ");//fim do menu
 		
 		scanf("%d", &opcao); //armazenamento a escolha do usuário
 		
 		system("cls"); //resposanvel por limpar a tela
 		
-		
-        switch(opcao) //inicio da seleção do menu
+		        switch(opcao) //inicio da seleção do menu
 	{
 			case 1:
 			registro(); //chamada de funções
@@ -150,6 +150,11 @@ int main()
 			deletar();
 			break;
 		
+		case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;
+			
 			default:
 			printf("Essa opção nao esta disponivel!\n");
 			system("pause");
